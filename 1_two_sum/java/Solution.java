dab47d05f3b46be947b547e971b7b514678e2520
fregdee <daniel.yuta@gmail.com>
2021-09-02 23:22:51 +0900

    1. Two Sum

---
+++ 1_two_sum/java/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+
+    static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (target == nums[i] + nums[j]) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
